befeba0d0349e20504e0cec8e9f3992e402195ce
Demons <www@yangblog.cn>
2019-12-08 23:52:10 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+)
 
 func main() {
+	if len(os.Args) <= 1 {
+		panic("You must specific a url.")
+	}
+	u := os.Args[1]
 	fmt.Println("Working...")
+	fmt.Println(parseUrl(u))
+}
+
+func parseUrl(u string) string {
+	res, err := url.Parse(u)
+	if err != nil {
+		panic(err)
+	}
+	if res.Scheme!="http" && res.Scheme!="https" {
+		panic("Protocol missing.")
+	}
+	if res.Host == "" {
+		panic("Host missing.")
+	}
+	if res.Port() == "80" {
+		log.Println("Chainsaw suggest you to remove default port 80, because this feature may affect the result.")
+	}
+	return res.Scheme+"://"+res.Host+"/"
 }
