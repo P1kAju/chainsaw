a6437591a5ba3a3af344fdb2c6176b1a773781dc
Demons <www@yangblog.cn>
2019-12-10 20:11:37 +0800

    add spring actuator information leak detect.

--- baseline/UnauthSystem.go
+++ baseline/UnauthSystem.go
@@ -7,6 +7,22 @@ import (
 	"strings"
 )
 
+func springActuator(u *string) bool {
+	list := [...]string{"/autoconfig", "/beans", "/env", "/configprops", "/dump", "/health", "/info", "/mappings", "/metrics", "/shutdown", "/trace"}
+	for _, l := range list {
+		entry := *u + l
+		resp, e := http.Get(entry)
+		if e != nil {
+			panic(e)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode == 200 {
+			log.Println("[*] Detected Spring Actuator information leak.", entry)
+		}
+	}
+	return false
+}
+
 func druid(u *string) bool {
 	entry := *u+"/druid/index.html"
 	resp, e := http.Get(entry)
