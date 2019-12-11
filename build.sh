8c2d16932d32ba57dd223670b812291bad4f5c9f
Demons <www@yangblog.cn>
2019-12-11 17:38:05 +0800

    add arm support.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o build/chainsaw_linux_386
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/chainsaw_linux_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o build/chainsaw_linux_arm
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o build/chainsaw_linux_arm64
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o build/chainsaw_windows_386.exe
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/chainsaw_windows_amd64.exe
 CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -o build/chainsaw_darwin_386
